d8427badc1084be2cb53e2109f4333e45ecbf6bb
tugboat62 <mujadded1998@gmail.com>
2023-12-07 03:39:31 +0600

    copied from book

--- Offline1/lcgrand.h
+++ Offline1/lcgrand.h
@@ -1,10 +1,12 @@
-#include <stdio.h>
-
 /* Define the constants. */
 #define MODLUS 2147483647
 #define MULT1 24112
 #define MULT2 26143
 /* Set the default seeds for all 100 streams. */
+
+float lcgrand(int stream);
+void lcgrandst(long zset, int stream);
+long lcgrandgt(int stream);
 
 static long zrng[] =
 { 1,
@@ -25,38 +27,32 @@ static long zrng[] =
 1923011392,1433700034,1244184613,1147297105, 539712780,1545929719,
 190641742,1645390429, 264907697, 620389253,1502074852, 927711160,
 364849192,2049576050, 638580085, 547070247 };
+
 /* Generate the next random number. */
 float lcgrand(int stream)
 {
-long zi, lowprd, hi31;
-zi = zrng[stream];
-lowprd = (zi & 65535) * MULT1;
-hi31 = (zi >> 16) * MULT1 + (lowprd >> 16);
-zi = ((lowprd & 65535) - MODLUS) +
-((hi31 & 32767) << 16) + (hi31 >> 15);
-if (zi < 0) zi += MODLUS;
-lowprd = (zi & 65535) * MULT2;
-hi31 = (zi >> 16) * MULT2 + (lowprd >> 16);
-zi = ((lowprd & 65535) - MODLUS) +
-((hi31 & 32767) << 16) + (hi31 >> 15);
-if (zi < 0) zi += MODLUS;
-zrng[stream] = zi;
-return (zi >> 7 | 1) / 16777216.0;
-}
-void lcgrandst (long zset, int stream) /* Set the current zrng for stream
-"stream" to zset. */
-{
-zrng[stream] = zset;
-}
-long lcgrandgt (int stream) /* Return the current zrng for stream "stream". */
-{
-return zrng[stream];
+    long zi, lowprd, hi31;
+    zi = zrng[stream];
+    lowprd = (zi & 65535) * MULT1;
+    hi31 = (zi >> 16) * MULT1 + (lowprd >> 16);
+    zi = ((lowprd & 65535) - MODLUS) +
+    ((hi31 & 32767) << 16) + (hi31 >> 15);
+    if (zi < 0) zi += MODLUS;
+    lowprd = (zi & 65535) * MULT2;
+    hi31 = (zi >> 16) * MULT2 + (lowprd >> 16);
+    zi = ((lowprd & 65535) - MODLUS) +
+    ((hi31 & 32767) << 16) + (hi31 >> 15);
+    if (zi < 0) zi += MODLUS;
+    zrng[stream] = zi;
+    return (zi >> 7 | 1) / 16777216.0;
 }
 
-int main() {
-    int i;
-    for (i = 0; i < 100; i++) {
-        printf("%f\n", lcgrand(i));
-    }
-    return 0;
+void lcgrandst (long zset, int stream) /* Set the current zrng for stream "stream" to zset. */
+{
+    zrng[stream] = zset;
+}
+
+long lcgrandgt (int stream) /* Return the current zrng for stream "stream". */
+{
+    return zrng[stream];
 }
